fa4954002b79012ae92114a715a9a30f2e1c768c
ravikumarsurya <92291375+ravikumarsurya@users.noreply.github.com>
2021-11-09 11:46:33 +0530

    Add files via upload

---
+++ three numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	printf("enter three numbers");
+	scanf("%d%d%d",&a,&b,&c);
+	printf("Enter 1st number");
+	scanf("%d",&a);
+	printf("Enter 2nd number");
+	scanf("%d",&b);
+	printf("Enter 3rd number");
+	scanf("%d",&c);
+	printf("number1:%d\n",a);
+	printf("number2:%d\n",b);
+	printf("number3:%d\n",c);
+}
